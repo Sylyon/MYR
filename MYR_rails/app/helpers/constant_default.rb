a29221e976a03a0e213a13b6a975055962c3636a
Sylyon <bastien.drouot@ensta-bretagne.org>
2015-03-04 16:21:36 +0100

    Reup generator

---
+++ MYR_rails/app/helpers/constant_default.rb
@@ -0,0 +1,43 @@
+module ConstantDefault
+
+  def myCSS
+	"layout/styles/layout.css".html_safe
+  end
+  def myLogo
+	"/stylesheets/images/default/logo.PNG".html_safe
+  end
+  def myBackGtroungImg
+	"/stylesheets/images/demo/backgrounds/1.png".html_safe
+  end
+  
+  ################################
+  def myTitle
+	"Default Title".html_safe
+  end
+  def myHome
+	"Home".html_safe
+  end
+  def myFollow
+	"Follow a regatta".html_safe
+  end
+  def myRealTime
+	"Real Time".html_safe
+  end
+  def myReplay
+	"Replay".html_safe
+  end
+  def myAccount
+	"My Account".html_safe
+  end
+  def myTeam
+	"My Team".html_safe
+  end
+  def myOwnInfo
+	"My Own Information".html_safe
+  end
+  def myContact
+	"Contact Us".html_safe
+  end
+  
+
+end
