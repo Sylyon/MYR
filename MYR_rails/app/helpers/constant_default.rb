13aa554fcc63c48021ab4e720b7fa1189425eccf
Sylyon <bastien.drouot@ensta-bretagne.org>
2015-03-04 18:31:37 +0100

    Generalise part 1 the home page

--- MYR_rails/app/helpers/constant_default.rb
+++ MYR_rails/app/helpers/constant_default.rb
@@ -11,6 +11,46 @@ module ConstantDefault
   end
   
   ################################
+  def myENSTA_Bretagne
+	"ENSTA Bretagne".html_safe
+  end
+  def myENSTA_Bretagne_shortText
+	"French Graduate and Post-graduate Engineering School and Research Institute based in Brest".html_safe
+  end
+  def myENSTA_Bretagne_logo
+	"/stylesheets/images/default/Logo_ENSTA_Bretagne.jpg".html_safe
+  end
+  def myENSTA_Bretagne_pach
+	"http://www.ensta-bretagne.eu/".html_safe
+  end
+  
+  def myAland
+	"Åland University".html_safe 
+  end
+  def myAland_shortText
+	"University of Applied Sciences based in Åland".html_safe
+  end
+  def myAland_logo
+	"/stylesheets/images/default/Logo_Aland2.png".html_safe
+  end
+  def myAland_pach
+	"http://www.ha.ax/text.con?iPage=28&iLan=1".html_safe
+  end
+  
+  def myWRSC
+	"WRSC".html_safe
+  end
+  def myWRSC_shortText
+	"World Robotic Sailing Championship".html_safe
+  end
+  def myWRSC_logo
+	"/stylesheets/images/default/Logo_WRSC.jpg".html_safe
+  end 
+  def myWRSC_pach
+	"http://wrsc2014.com/".html_safe
+  end
+  
+
   def myTitle
 	"Default Title".html_safe
   end
@@ -20,12 +60,21 @@ module ConstantDefault
   def myFollow
 	"Follow a regatta".html_safe
   end
+  
   def myRealTime
 	"Real Time".html_safe
   end
+  def myRealTime_shortText
+	"Click here to follow the WRSC 2015".html_safe
+  end
+  
   def myReplay
 	"Replay".html_safe
   end
+  def myReplay_shortText
+	"Click here to replay an event of the WRSC 2015".html_safe
+  end  
+  
   def myAccount
 	"My Account".html_safe
   end
