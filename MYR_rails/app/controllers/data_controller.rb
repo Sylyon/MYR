5e200ea4f6972a30c78ba471e97c91ceb2f99e44
Benoit Bourdon <chinchifou@gmail.com>
2015-03-05 14:17:49 +0100

    Add properly the new created coordinates
    Improve readibility of the code

--- MYR_rails/app/controllers/data_controller.rb
+++ MYR_rails/app/controllers/data_controller.rb
@@ -2,8 +2,12 @@ class DataController < ApplicationController
   def create
   	if params[:datetime] != nil
 
-  		newCoords = Coordinate.where "datetime > ?", params[:datetime]
-  		render json: newCoords.to_json
+
+  		@newCoords = Coordinate.where "datetime > ?", params[:datetime]
+
+      render json: @newCoords
+  		
+      #render json: params[:datetime]
 
   	end
   end
