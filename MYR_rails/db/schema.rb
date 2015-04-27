70251e033fbda4dae3c241c9e7acc287b0beb98f
Sylyon <bastien.drouot@ensta-bretagne.org>
2015-04-27 09:38:06 +0200

    add favicon

--- MYR_rails/db/schema.rb
+++ MYR_rails/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150312092135) do
-
 
   create_table "attempts", force: :cascade do |t|
     t.string   "name"
