b246b9d49700e3b863cf432bc18f4a3a0c28d658
Sylyon <bastien.drouot@ensta-bretagne.org>
2015-05-18 09:20:57 +0200

    Revert "markers alerte test"
    This reverts commit 5d93db997f9f13283d55aa6330b380ad63bd5356.

--- MYR_rails/app/assets/javascripts/markersCreation.js
+++ MYR_rails/app/assets/javascripts/markersCreation.js
@@ -1,39 +1,7 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
 	
-	new_marker();
-
+	initializeMap();
+	initialScroll();
+	addingBoil();
 
 });
-
-function new_marker(){
-	console.log("new_marker");
-	jQuery("#edit_element_dialog").dialog({
-		autoOpen: true,
-		modal: true,
-		width: 400,
-		height:200,
-		title: 'blabla0',
-		open : function(){
-
-			jQuery("#edit_element_dialog").html('<p>blabla1</p><br/><p>blalba2</p><br/><table><tr><td align="right">blabla3 : </td><td align="center"><input type="text" name="mission_name" value=""/></td></tr></table>');
-
-		},
-		buttons: {
-			"blabla4": function(){
-				var mission_name = jQuery("input[name=mission_name]").val();
-				if(mission_name!="") // Si l'utilisateur n'écrit rien, le bouton "confirmer" ne fait rien
-				{
-					console.log("mission selected >?");
-						initializeMap();
-						initialScroll();
-						addingBoil();
-				}
-
-			},
-			"blabla5": function(){
-				jQuery( this ).dialog( "close" );
-			}
-
-		}
-	});
-}
