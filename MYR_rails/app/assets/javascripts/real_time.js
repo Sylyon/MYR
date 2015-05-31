08cad0ec6323455d05cbce1e9b0d425691f015fb
Benoit Bourdon <chinchifou@gmail.com>
2015-05-31 19:16:01 +0100

    Fix performance issue
    > problem due to synchronous Jquery call inside a Rails partial

--- MYR_rails/app/assets/javascripts/real_time.js
+++ MYR_rails/app/assets/javascripts/real_time.js
@@ -39,29 +39,25 @@ $(document).ready(function(){
 		});
 	});
 
-	$("#map-panel").ready(function(){
-		alert("hello");
+});
 
-$("#map-panel").on("click", "input[name*='tracker']", function() {
-  //for all checkboxes of tracker
-	//$( "input[name*='tracker']" ).each(function () {  
-    //on click do ...
-    $(this).click(function() {
+//when the panel is displayed
+$("#map-panel").ready(function(){
+
+  //for all checkboxes of tracker, on click do ...
+  $("#map-panel").on("click", "input[name*='tracker']", function() {
       //get id of the checkbox
       var id = $(this).attr('id');
-      alert("My ID is "+id);
       //if checked
       if($(this).is(':checked')){
-        saveNewDesiredTracker(id);
+      	saveNewDesiredTracker(id);
       }
       //if not checked
       else{
-        removeDesiredTracker(id);
+      	removeDesiredTracker(id);
       } 
-    })
-  })
 
-});
+    })
 
 });
 
