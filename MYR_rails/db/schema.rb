fb2a888685ddae38443f17a09e9e900f33f3b0ca
nicolas2lee <nicolas2lee@gmail.com>
2015-06-16 09:57:50 +0200

    Need to fix ajax button (by using button_to?)

--- MYR_rails/db/schema.rb
+++ MYR_rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312092135) do
+ActiveRecord::Schema.define(version: 20150616065900) do
 
   create_table "attempts", force: :cascade do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150312092135) do
     t.integer  "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "tracker_id"
   end
 
   create_table "sessions", force: :cascade do |t|
